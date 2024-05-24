8f8908d65ff82d4dfb630425b56ff1a58d5e2be3
Changjun Yeo <lunaradio24@gmail.com>
2024-05-24 20:02:14 +0900

    Fix: Now refresh token is issued only when there is no existing refresh token in DB

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -24,7 +24,7 @@ function createRefreshToken(userId) {
   const refreshToken = jwt.sign(
     { id: userId },
     process.env.REFRESH_TOKEN_SECRET_KEY,
-    { expiresIn: '7d' },
+    { expiresIn: '7s' },
   );
   return refreshToken;
 }
@@ -120,61 +120,86 @@ router.post('/auth/sign-up', async (req, res, next) => {
 
 /*****     로그인 API     *****/
 router.post('/auth/sign-in', async (req, res, next) => {
-  // 1. 요청 정보 - 이메일, 비밀번호를 Request Body(`req.body`)로 전달 받습니다.
-  const { email, password } = req.body;
+  try {
+    // 1. 요청 정보 - 이메일, 비밀번호를 Request Body(`req.body`)로 전달 받습니다.
+    const { email, password } = req.body;
 
-  // 2. 유효성 검증 및 에러 처리
-  //  - 로그인 정보 중 하나라도 빠진 경우 - “OOO을 입력해 주세요.”
-  if (!email) {
-    return res.status(400).json({ errorMessage: 'email을 입력해 주세요.' });
-  }
-  if (!password) {
-    return res.status(400).json({ errorMessage: '비밀번호을 입력해 주세요.' });
-  }
-  //  - 이메일 형식에 맞지 않는 경우 - “이메일 형식이 올바르지 않습니다.”
-  const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-  if (!email_regex.test(email)) {
+    // 2. 유효성 검증 및 에러 처리
+    //  - 로그인 정보 중 하나라도 빠진 경우 - “OOO을 입력해 주세요.”
+    if (!email) {
+      return res.status(400).json({ errorMessage: 'email을 입력해 주세요.' });
+    }
+    if (!password) {
+      return res
+        .status(400)
+        .json({ errorMessage: '비밀번호을 입력해 주세요.' });
+    }
+    //  - 이메일 형식에 맞지 않는 경우 - “이메일 형식이 올바르지 않습니다.”
+    const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!email_regex.test(email)) {
+      return res
+        .status(400)
+        .json({ errorMessage: '이메일 형식이 올바르지 않습니다.' });
+    }
+    //  - 이메일로 조회되지 않거나 비밀번호가 일치하지 않는 경우 - “인증 정보가 유효하지 않습니다.”
+    const user = await prisma.users.findUnique({
+      where: { email: email },
+    });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errorMessage: '인증 정보가 유효하지 않습니다.' });
+    }
+    const pwMatch = await bcrypt.compare(password, user.password);
+    if (!pwMatch) {
+      return res
+        .status(400)
+        .json({ errorMessage: '인증 정보가 유효하지 않습니다.' });
+    }
+
+    // 3. 비즈니스 로직(데이터 처리)
+    //Access, Refresh Token 발급 - Payload에 `사용자 ID`를 포함하고, 유효기한이 `12시간`
+    const accessToken = createAccessToken(user.userId);
+    const refreshToken = createRefreshToken(user.userId);
+
+    //DB의 refreshTokens 테이블에 Refresh Token이 이미 있으면 저장 X
+    const existingToken = await prisma.refreshTokens.findFirst({
+      where: { UserId: user.userId },
+    });
+    console.log(existingToken);
+    //없다면, 새로 발급한 Refresh Token을 DB에 저장
+    if (!existingToken) {
+      await prisma.refreshTokens.create({
+        data: {
+          tokenId: refreshToken,
+          UserId: user.userId,
+          ip: req.ip,
+          userAgent: req.headers['user-agent'],
+        },
+      });
+    }
+    //있다면, 새로 발급한 Refresh Token으로 갱신
+    else {
+      await prisma.refreshTokens.update({
+        where: { tokenId: existingToken.tokenId },
+        data: {
+          tokenId: refreshToken,
+          ip: req.ip,
+          userAgent: req.headers['user-agent'],
+        },
+      });
+    }
+    // 4. 반환 정보 - AccessToken, RefreshToken을 반환합니다.
+    //클라이언트에게 쿠키(토큰)을 할당
+    res.cookie('accessToken', `Bearer ${accessToken}`);
+    res.cookie('refreshToken', `Bearer ${refreshToken}`);
+
     return res
-      .status(400)
-      .json({ errorMessage: '이메일 형식이 올바르지 않습니다.' });
+      .status(201)
+      .json({ message: '토큰이 정상적으로 발급되었습니다.' });
+  } catch (error) {
+    next(error);
   }
-  //  - 이메일로 조회되지 않거나 비밀번호가 일치하지 않는 경우 - “인증 정보가 유효하지 않습니다.”
-  const user = await prisma.users.findUnique({
-    where: { email: email },
-  });
-  if (!user) {
-    return res
-      .status(400)
-      .json({ errorMessage: '인증 정보가 유효하지 않습니다.' });
-  }
-  const pwMatch = await bcrypt.compare(password, user.password);
-  if (!pwMatch) {
-    return res
-      .status(400)
-      .json({ errorMessage: '인증 정보가 유효하지 않습니다.' });
-  }
-
-  // 3. 비즈니스 로직(데이터 처리)
-  //Access, Refresh Token 발급 - Payload에 `사용자 ID`를 포함하고, 유효기한이 `12시간`
-  const accessToken = createAccessToken(user.userId);
-  const refreshToken = createRefreshToken(user.userId);
-
-  //DB의 refreshTokens 테이블에 Refresh Token 정보 저장
-  await prisma.refreshTokens.create({
-    data: {
-      tokenId: refreshToken,
-      UserId: user.userId,
-      ip: req.ip,
-      userAgent: req.headers['user-agent'],
-    },
-  });
-
-  // 4. 반환 정보 - AccessToken, RefreshToken을 반환합니다.
-  //클라이언트에게 쿠키(토큰)을 할당
-  res.cookie('accessToken', `Bearer ${accessToken}`);
-  res.cookie('refreshToken', `Bearer ${refreshToken}`);
-
-  return res.status(201).json({ message: '토큰이 정상적으로 발급되었습니다.' });
 });
 
 export default router;
--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -59,4 +59,36 @@ router.post('/resumes', authorizeAccessToken, async (req, res, next) => {
   }
 });
 
+/*****     이력서 목록 조회 API     *****/
+router.get(
+  '/resumes/:userId?query',
+  authorizeAccessToken,
+  async (req, res, next) => {
+    try {
+      // 1. 요청 정보
+      //     - 사용자 정보는 인증 Middleware(`req.user`)를 통해서 전달 받습니다.
+      const { userId } = req.user;
+      //     - Query Parameters(`req.query`)으로 정렬 조건을 받습니다.
+      const { query } = req.query;
+      console.log(query);
+      const sortOption = query.split('=')[1];
+      console.log(sortOption);
+      //     - 생성일시 기준 정렬은 `과거순(ASC),` `최신순(DESC)`으로 전달 받습니다. 값이 없는 경우 `최신순(DESC)` 정렬을 기본으로 합니다. 대소문자 구분 없이 동작해야 합니다.
+      //     - 예) `sort=desc`
+
+      // 2. 유효성 검증 및 에러 처리
+      //     - 일치하는 값이 없는 경우 - 빈 배열(`[]`)을 반환합니다. (StatusCode: 200)
+      // 3. 비즈니스 로직(데이터 처리)
+      //     - 현재 로그인 한 사용자가 작성한 이력서 목록만 조회합니다.
+      //     - DB에서 이력서 조회 시 작성자 ID가 일치해야 합니다.
+      //     - 정렬 조건에 따라 다른 결과 값을 조회합니다.
+      //     - 작성자 ID가 아닌 작성자 이름을 반환하기 위해 스키마에 정의 한 Relation을 활용해 조회합니다.
+      // 4. 반환 정보
+      //     - 이력서 ID, 작성자 이름, 제목, 자기소개, 지원 상태, 생성일시, 수정일시의 목록을 반환합니다.
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 export default router;
